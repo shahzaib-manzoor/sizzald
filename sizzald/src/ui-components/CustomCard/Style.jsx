0eb70778961f96b086ac7aa9c706b5a1ffe8f782
malikjunaidhassan <122723695+malikjunaidhassan@users.noreply.github.com>
2023-01-19 15:24:10 +0500

    implementing carousel

---
+++ sizzald/src/ui-components/CustomCard/Style.jsx
@@ -0,0 +1,48 @@
+import PromCard1 from "../../assets/Prom-card1.png";
+
+export const cardContainer = { padding: "40px 0px" };
+export const cardMain = {
+  //   maxWidth: "420px",
+  margin: "0 20px",
+  background: `url(${PromCard1}) center/cover`,
+  // backgroundSize: "cover",
+  // objectFit: "center",
+  position: "relative",
+  padding: "0px 0px",
+  display: "flex",
+  justifyContent: "space-around",
+  alignContent: "space-between",
+  alignItems: "baseline",
+};
+export const cardContent = {
+  display: "flex",
+  height: "250px",
+  alignItems: " flex-start",
+  flexDirection: "column",
+  justifyContent: "flex-end",
+  padding: "10px 0px",
+};
+export const cardContentText = {
+  color: "white",
+  fontSize: "18px",
+  fontWeight: "600",
+  lineHeight: "20px",
+};
+export const cardContentTextVS = {
+  color: "#03FB75",
+  fontSize: "18px",
+  fontWeight: "600",
+  lineHeight: "25px",
+  padding: "0px 6px",
+};
+export const cardContentTextWar = {
+  color: "white",
+  fontSize: "18px",
+  fontWeight: "600",
+  lineHeight: "25px",
+  letterSpacing: "0.30000001192092896px",
+};
+export const cardContentIcon = {
+  color: "white",
+};
+export const cardContentIconSize = { fontSize: "14px" };
