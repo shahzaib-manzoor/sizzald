0569e16cedda237450497860682225a1c5795976
malikjunaidhassan <122723695+malikjunaidhassan@users.noreply.github.com>
2023-01-20 20:11:26 +0500

    implementing navColor

--- sizzald/src/layout/Header/index.jsx
+++ sizzald/src/layout/Header/index.jsx
@@ -168,8 +168,8 @@ export default function PrimarySearchAppBar() {
   );
 
   return (
-    <Box className="!bg-clBlueContainer" sx={style.containerNav}>
-      <AppBar className="!bg-clBlue " position="fixed">
+    <Box sx={style.containerNav}>
+      <AppBar sx={{ backgroundColor: "#75788D" }} position="fixed">
         <Toolbar sx={style.Toolbar}>
           <IconButton size="large" color="inherit">
             <MenuIcon />
