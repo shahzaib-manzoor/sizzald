65fca8492d74f7e40600bf4e244701f1e255ddb7
malikjunaidhassan <122723695+malikjunaidhassan@users.noreply.github.com>
2023-01-26 17:07:33 +0500

    tj push

--- sizzald/src/layout/index.jsx
+++ sizzald/src/layout/index.jsx
@@ -10,9 +10,9 @@ import MiniSideBar from "./SideBar/MiniSideBar";
 const Layout = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
   const [mobileDrawer, setmobileDrawer] = useState(false);
-  const isMediumScreen = useMediaQuery('(max-width: 960px)')
-  const iSLargeScreen = useMediaQuery('(min-width: 960px)')
-  const isSmallScreen = useMediaQuery('(max-width: 600px)')
+  const isMediumScreen = useMediaQuery("(max-width: 960px)");
+  const iSLargeScreen = useMediaQuery("(min-width: 960px)");
+  const isSmallScreen = useMediaQuery("(max-width: 600px)");
   const handleDrawer = () => {
     setOpenDrawer(!openDrawer);
   };
@@ -22,26 +22,29 @@ const Layout = () => {
   const handlemObileDrawer = () => {
     setmobileDrawer(!mobileDrawer);
   };
-// console.log(isMediumScreen)
+  // console.log(isMediumScreen)
   //  ui
-  const sideBar={
+  const sideBar = {
     width: openDrawer
-    ? "7%"
-    : mobileDrawer
-    ? "100%"
-    : { xs: "100%", sm: "12%", md: "0%", lg: "20%" },
-  // mt: "30px",
-  display: { xs: mobileDrawer ? "block" : "none", lg: "block",sm:'block' },
-}
-const outLet={
-  width: { xs: "100%",sm:openDrawer?'93%':"88%", lg: openDrawer ? "93%" : "80%" },
-  // mt: "30px",
-  display: "flex",
-  flexDirection: "column",
-  
-}
-console.log(openDrawer,iSLargeScreen)
-  // console.log(window.innerWidth)
+      ? "7%"
+      : mobileDrawer
+      ? "100%"
+      : { xs: "100%", sm: "12%", md: "0%", lg: "20%" },
+    // mt: "30px",
+    display: { xs: mobileDrawer ? "block" : "none", lg: "block", sm: "block" },
+  };
+  const outLet = {
+    width: {
+      xs: "100%",
+      sm: openDrawer ? "93%" : "88%",
+      lg: openDrawer ? "93%" : "80%",
+    },
+    // mt: "30px",
+    display: "flex",
+    flexDirection: "column",
+  };
+  console.log(openDrawer, iSLargeScreen);
+  console.log(window.innerWidth);
   return (
     <div>
       <Header
@@ -49,45 +52,52 @@ console.log(openDrawer,iSLargeScreen)
         handlemObileDrawer={handlemObileDrawer}
       />
       <Box sx={{ display: "flex" }}>
-        <Box sx={sideBar} >
-
-          {
-            iSLargeScreen && (
-              <>
-              {openDrawer  ? (
+        <Box sx={sideBar}>
+          {iSLargeScreen && (
+            <>
+              {openDrawer ? (
                 <MiniSideBar width={openDrawer ? "5%" : "0%"} />
               ) : (
                 <SideBar width={openDrawer ? "20%" : "0%"} />
               )}
-              </>)
-           }
-           {
-            isMediumScreen && (
-              <>
-              {openDrawer  ? (
+            </>
+          )}
+          {isMediumScreen && (
+            <>
+              {openDrawer ? (
                 <SideBar width={openDrawer ? "20%" : "0%"} />
-                ) : (
+              ) : (
                 <MiniSideBar width={openDrawer ? "7%" : "0%"} />
               )}
-              </>)
-           }
-           {
-            isSmallScreen && (
-              <>
-              {!openDrawer  ? (
-                <SideBar width={openDrawer ? "20%" : "0%"} />
-                ) : <></>
+            </>
+          )}
+          {isSmallScreen && (
+            <>
+              {
+                !openDrawer ? (
+                  <SideBar width={openDrawer ? "20%" : "0%"} />
+                ) : (
+                  <></>
+                )
                 // <MiniSideBar width={openDrawer ? "7%" : "0%"} />
               }
-              </>)
-           }
+            </>
+          )}
         </Box>
-        <Box sx={outLet} >
-          <Box sx={{display:{xs:mobileDrawer?"none":'block',sm:'block'}}}>
-          <Outlet />
+        <Box sx={outLet}>
+          <Box
+            sx={{
+              display: { xs: mobileDrawer ? "none" : "block", sm: "block" },
+            }}
+          >
+            <Outlet />
           </Box>
           <Footer />
-          <Box sx={{display:{xs:'block',sm:"none",md:"none",lg:'none'}}}>
+          <Box
+            sx={{
+              display: { xs: "block", sm: "none", md: "none", lg: "none" },
+            }}
+          >
             <MobileFoter handleMObileDrawer={handleMObileDrawer} />
           </Box>
         </Box>
@@ -96,14 +106,3 @@ console.log(openDrawer,iSLargeScreen)
   );
 };
 export default Layout;
-
-
-
-
-
-
-
-
-
-
-
