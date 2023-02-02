1f66ae01b6a4ace82b96adbb238b85aa5a94b583
asma-ciel <asma@cielrecruiter.com>
2023-02-02 19:08:06 +0500

    affilate compelete

--- sizzald/src/components/Affilate/index.jsx
+++ sizzald/src/components/Affilate/index.jsx
@@ -1,12 +1,16 @@
 // import React from 'react'
 import { Container } from '@mui/material';
+import SectionFiveCard from './SectionFiveCard';
 import SectionFour from './SectionFour';
+import SectionTabel from './SectionTabel';
 // import affilateRocket from '../../../../../../../assets/affilateRocket'
 
 const index = () => {
   return (
     <Container maxWidth='xl'>
      <SectionFour/>
+     <SectionFiveCard/>
+     <SectionTabel/>
     </Container>
   );
 };
