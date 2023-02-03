97a124eedc70e3bb536b23ce8b42ef87562f2c7e
malikjunaidhassan <122723695+malikjunaidhassan@users.noreply.github.com>
2023-02-03 09:35:45 +0500

    affiliate section

--- sizzald/src/components/Affilate/index.jsx
+++ sizzald/src/components/Affilate/index.jsx
@@ -1,12 +1,18 @@
 // import React from 'react'
-import { Container } from '@mui/material';
-import SectionFour from './SectionFour';
+import { Container } from "@mui/material";
+import Advertisment from "./Advertisment";
+import AffiliateReward from "./AffiliateReward";
+import EarningCard from "./EarningCard";
+import SectionFour from "./SectionFour";
 // import affilateRocket from '../../../../../../../assets/affilateRocket'
 
 const index = () => {
   return (
-    <Container maxWidth='xl'>
-     <SectionFour/>
+    <Container maxWidth="xl">
+      <Advertisment />
+      <EarningCard />
+      <AffiliateReward />
+      <SectionFour />
     </Container>
   );
 };
