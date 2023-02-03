6294755fb089381f1e398f76697f4fb35d304f30
shahzaib <shahzi113awan@gmail.com>
2023-02-03 20:52:10 +0500

    ka

--- server/index.js
+++ server/index.js
@@ -30,6 +30,10 @@ app.get("*", (req, res) => {
   app.use(express.static(path.join(__dirname, "/dist/index.html")));
 
 });
+app.get("/signup", (req, res) => {
+  app.use(express.static(path.join(__dirname, "/dist/index.html")));
+
+});
 
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
